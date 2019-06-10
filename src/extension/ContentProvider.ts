276f6b556c29682d2cd0e45d3999e420d19a4809
R. G. Esteves <rgesteves@gmail.com>
2019-06-10 00:56:02 -0700

    Open webview and display contents

---
+++ src/extension/ContentProvider.ts
@@ -0,0 +1,22 @@
+import { Uri, ExtensionContext, Extension } from 'vscode';
+import { join } from 'path';
+
+export default class ContentProvider {
+    getProdContent(context : ExtensionContext ) {
+        const unBundleDiskPath = Uri.file(join(context.extensionPath, "out", "webview", "webview.bundle.js"));
+        const unBundlePath = unBundleDiskPath.with({ scheme: 'vscode-resource'});
+        return `
+        <!doctype html>
+        <html lang="en">
+          <head>
+            <meta charset="utf-8">
+            <title>Model validation comparison</title>
+          </head>
+          <body>
+            <div id="root"></div>
+            <script src="${unBundlePath}" type="text/javascript"></script>
+          </body>
+        </html>
+        `;
+    }
+}
