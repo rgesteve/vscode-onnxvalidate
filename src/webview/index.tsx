ea4f16fcb1595f44225d5bab0595ea81605f5eb8
R. G. Esteves <rgesteves@gmail.com>
2019-06-13 07:49:06 -0700

    Layout based on office-ui Stack

--- src/webview/index.tsx
+++ src/webview/index.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { TextField } from "office-ui-fabric-react/lib/TextField";
-import { PrimaryButton } from "office-ui-fabric-react/lib/Button";
+
+import { Stack, TextField, PrimaryButton } from "office-ui-fabric-react";
 
 
 const App = () => {
+
+    const tokens = {
+        fiveGapStack : {
+            childrenGap : 5
+        }
+    };
+
     return (
         <div>
-            <p>Hello, world (changed)!</p>
-            <TextField value="This is a test" placeholder="Test..." />
-            <p>There should be a text field above this line and a button beneath</p>
-            <PrimaryButton type="submit">Contents of Button</PrimaryButton>
+            <Stack tokens={tokens.fiveGapStack}>
+                <Stack.Item align="center">
+                    <span>Hello, world (inside a stack)!</span>
+                    <TextField value="Should be centered" placeholder="Test..." />
+                </Stack.Item>
+                <Stack.Item align="stretch">
+                    <span>There should be a button besides</span>
+                    <PrimaryButton type="submit">Should be mocking file-open</PrimaryButton>
+                </Stack.Item>
+            </Stack>
         </div>
     );
 };
