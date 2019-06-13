4ffd4ad8ff884d84a24bcae489bd969c8c770ccd
R. G. Esteves <rgesteves@gmail.com>
2019-06-13 01:44:41 -0700

    Added dependency on office-ui-fabric-react

--- src/webview/index.tsx
+++ src/webview/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { TextField } from "office-ui-fabric-react/lib/TextField";
+import { PrimaryButton } from "office-ui-fabric-react/lib/Button";
+
 
 const App = () => {
     return (
         <div>
-            <p>Hello, world!</p>
+            <p>Hello, world (changed)!</p>
+            <TextField value="This is a test" placeholder="Test..." />
+            <p>There should be a text field above this line and a button beneath</p>
+            <PrimaryButton type="submit">Contents of Button</PrimaryButton>
         </div>
     );
 };
